4818a218b8c3c455767f74cf2207d0d65a4c0909
Tommy Thorn <tommy-github2@thorn.ws>
2024-09-09 17:03:38 -0700

    Factored into structs

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
+# drawhats
+
+
 ![Four hat tiling](four-hats.png)
 
-# Drawing the "Hat" tiling
+## Drawing the "Hat" tiling
 
 I'm fashinated by [the Hat](https://en.wikipedia.org/wiki/Einstein_problem)
 
@@ -20,3 +23,47 @@ About 90% of the time on this code was figuring out how to print them.
 Currently the graphics only works if you launch this from an iTerm2
 terminal.  That's trivial to fix.
 
+
+
+
+## Find Hat tilings
+
+TL;DR: an astonishing breakthough in irregular tilings occurred in 2022 [1,2,3] when David Smith
+found the "Hat".  In this crate I aim to tile rectangles with the Hat, from *1st principle*.
+
+- [1] [Up and Atom's entertaining coverage](https://www.youtube.com/watch?v=A1BhOVW8qZU)
+- [2] [Wikipedia entry](https://en.wikipedia.org/wiki/Einstein_problem)
+- [3] [Sci. Am. has a great story on this](https://www.scientificamerican.com/article/newfound-mathematical-einstein-shape-creates-a-never-repeating-pattern/)
+
+The Hat is made up of eight "kites", where a kite is the six part of a hexagon you get from
+dividing with the three lines that intersect the center and midpoints of the sides.  Thus if we
+wish to tile our rectangle ("plane") with Hats then must first find a way tile it with kites.
+Lacking better ideas, I tile my rectangle with hexagons and refer to the kites within by orientation
+(0 being the 1 O'clock kites, 1, being the 3 O'olock etc.)
+
+First, map hexagons to a grid
+
+ ```
+      >---<         >---<         >
+     /     \       /     \       /
+    /  0 0  \     /  2 0  \     /
+   <    +    >---<    +    >---<
+    \       /     \       /     \
+     \     /  1 0  \     /  3 0  \
+      >---<    +    >---<    +    >
+     /     \       /     \       /
+    /  0 1  \     /  2 1  \     /
+   <    +    >---<    +    >---<
+    \       /     \       /     \
+     \     /  1 1  \     /  3 1  \
+      >---<    +    >---<    +    >
+     /     \       /     \       /
+    /  0 2  \     /  2 2  \     /
+   <    +    >---<    +    >---<
+    \       /     \       /     \
+     \     /  1 2  \     /  3 2  \
+      >---<    +    >---<    +    >
+ ```
+which gives rise to a funny neigbor offsets:
+- even columns: (0,-1), (1, -1), (1, 0), (0,1), (-1, 0), (-1, -1)
+- odd columns:  (0,-1), (1,  0), (1, 1), (0,1), (-1, 1), (-1,  0)
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,92 @@
-/*
- * First, map hexagons to a grid
- *
- *     >---<         >---<         >
- *    /     \       /     \       /
- *   /  0 0  \     /  2 0  \     /
- *  <    +    >---<    +    >---<
- *   \       /     \       /     \
- *    \     /  1 0  \     /  3 0  \
- *     >---<    +    >---<    +    >
- *    /     \       /     \       /
- *   /  0 1  \     /  2 1  \     /
- *  <    +    >---<    +    >---<
- *   \       /     \       /     \
- *    \     /  1 1  \     /  3 1  \
- *     >---<    +    >---<    +    >
- *    /     \       /     \       /
- *   /  0 2  \     /  2 2  \     /
- *  <    +    >---<    +    >---<
- *   \       /     \       /     \
- *    \     /  1 2  \     /  3 2  \
- *     >---<    +    >---<    +    >
- *
- * which gives rise to a funny neigbor offsets:
- * even columns: (0,-1), (1, -1), (1, 0), (0,1), (-1, 0), (-1, -1)
- * odd columns:  (0,-1), (1,  0), (1, 1), (0,1), (-1, 1), (-1,  0)
- */
+/*!
 
-use iterm2canvas::Pict;
+![Four hat tiling](four-hats.png)
 
-// Grid width and height
+# Drawing the "Hat" tiling
 
-const WHITE: u32 = 0xFFFFFF;
-const GREY: u32 = 0x7F7F7F;
-const YELLOW: u32 = 0xFFFF00;
+I'm fashinated by [the Hat](https://en.wikipedia.org/wiki/Einstein_problem)
 
-const N: isize = 7;
+Here's my from-1st-principle attempt at solving it.
 
+Note, the code is atrociously bad, but optimization is straight
+forward (?) once the code is working.  Currently the search is really
+unacceptible naive.
+
+The first problem is how to represent the world.  What I chose for
+now, is to tile the plane with hexagons, subdivided into kites (the
+Hat consists of eight connected kites).  The hexagon tiling is using
+standard grid mapping where odd rows are offset by half a hexagon
+(there are many other options).
+
+About 90% of the time on this code was figuring out how to print them.
+Currently the graphics only works if you launch this from an iTerm2
+terminal.  That's trivial to fix.
+
+
+
+
+# Find Hat tilings
+
+TL;DR: an astonishing breakthough in irregular tilings occurred in 2022 [1,2,3] when David Smith
+found the "Hat".  In this crate I aim to tile rectangles with the Hat, from *1st principle*.
+
+- [1] [Up and Atom's entertaining coverage](https://www.youtube.com/watch?v=A1BhOVW8qZU)
+- [2] [Wikipedia entry](https://en.wikipedia.org/wiki/Einstein_problem)
+- [3] [Sci. Am. has a great story on this](https://www.scientificamerican.com/article/newfound-mathematical-einstein-shape-creates-a-never-repeating-pattern/)
+
+The Hat is made up of eight "kites", where a kite is the six part of a hexagon you get from
+dividing with the three lines that intersect the center and midpoints of the sides.  Thus if we
+wish to tile our rectangle ("plane") with Hats then must first find a way tile it with kites.
+Lacking better ideas, I tile my rectangle with hexagons and refer to the kites within by orientation
+(0 being the 1 O'clock kites, 1, being the 3 O'olock etc.)
+
+First, map hexagons to a grid
+
+ ```
+      >---<         >---<         >
+     /     \       /     \       /
+    /  0 0  \     /  2 0  \     /
+   <    +    >---<    +    >---<
+    \       /     \       /     \
+     \     /  1 0  \     /  3 0  \
+      >---<    +    >---<    +    >
+     /     \       /     \       /
+    /  0 1  \     /  2 1  \     /
+   <    +    >---<    +    >---<
+    \       /     \       /     \
+     \     /  1 1  \     /  3 1  \
+      >---<    +    >---<    +    >
+     /     \       /     \       /
+    /  0 2  \     /  2 2  \     /
+   <    +    >---<    +    >---<
+    \       /     \       /     \
+     \     /  1 2  \     /  3 2  \
+      >---<    +    >---<    +    >
+ ```
+which gives rise to a funny neigbor offsets:
+- even columns: (0,-1), (1, -1), (1, 0), (0,1), (-1, 0), (-1, -1)
+- odd columns:  (0,-1), (1,  0), (1, 1), (0,1), (-1, 1), (-1,  0)
+*/
+
+type KiteColor = u8;
+const EMPTY: KiteColor = 0;
+
+// Half the width of a display hexagon
 const RADIUS: isize = 32;
 
 #[derive(Copy, Clone, PartialEq, Debug)]
-struct Hex(isize, isize);
+struct HexPos(isize, isize);
 
-type KiteColor = usize;
-const EMPTY: KiteColor = 0;
-
-impl Hex {
-    fn screen(self) -> (isize, isize) {
-        let y = self.1 * RADIUS * 2 + (if self.0 % 2 == 1 { RADIUS } else { 0 });
-        let x = self.0 * RADIUS * 15 / 10;
+impl HexPos {
+    fn display_center(self) -> (isize, isize) {
+        let y = RADIUS * (self.1 * 2 + 1 + self.0 % 2);
+        let x = RADIUS * (self.0 * 2 + 1) * 866 / 1000; // 2cos(30)
 
         (x, y)
     }
 
-    fn neighbor(self, direction: usize) -> Hex {
-        let Hex(x, y) = self;
+    fn neighbor(self, direction: usize) -> HexPos {
+        let HexPos(x, y) = self;
         let odd = if x % 2 == 1 { 1 } else { 0 };
         let (xd, yd) = match direction {
             0 => (0, -1),
@@ -65,7 +98,7 @@ impl Hex {
             _ => panic!("Illegal direction {direction}, must be [0..5]"),
         };
 
-        Hex(x + xd, y + yd)
+        HexPos(x + xd, y + yd)
     }
 }
 
@@ -75,7 +108,7 @@ mod test {
     fn all_neighbors(h: (isize, isize)) -> Vec<(isize, isize)> {
         (0..6)
             .map(|d| {
-                let n = Hex(h.0, h.1).neighbor(d);
+                let n = HexPos(h.0, h.1).neighbor(d);
                 (n.0, n.1)
             })
             .collect()
@@ -102,209 +135,172 @@ fn avg3(p1: (isize, isize), p2: (isize, isize), p3: (isize, isize)) -> (isize, i
     ((p1.0 + p2.0 + p3.0) / 3, (p1.1 + p2.1 + p3.1) / 3)
 }
 
-fn plot_hex(hex: &[[[KiteColor; 6]; N as usize]; N as usize]) {
-    let mut pict = Pict::new((N * 2 + 1) * RADIUS, (N * 2 + 1) * RADIUS);
+struct KiteGrid<const N: usize> {
+    // Note, rounded 6 up to 8 because alignment is nice.
+    hex: [[[KiteColor; 8]; N]; N],
+}
 
-    // I just need the center point of hexagon h
-    // The midpoint between the neighbor h' and h AVG(h',h) is the middle of the line between them.
-    // Call the left neighbor of h and h' L(h,h') and R(h,h') then the avg of h, h', and L(
+impl<const N: usize> KiteGrid<N> {
+    fn valid(&self, h: HexPos) -> bool {
+        let HexPos(x, y) = h;
+        (0..N as isize).contains(&x) && (0..N as isize).contains(&y)
+    }
 
-    for y in 0..N {
-        for x in 0..N {
-            let h = Hex(x, y);
+    fn get(&self, h: HexPos, kite: usize) -> KiteColor {
+        let HexPos(x, y) = h;
+        self.hex[x as usize][y as usize][kite]
+    }
 
-            pict.plot(h.screen(), WHITE);
-            let center = h.screen();
+    fn set(&mut self, h: HexPos, kite: usize, c: KiteColor) {
+        let HexPos(x, y) = h;
+        self.hex[x as usize][y as usize][kite] = c;
+    }
 
-            for kite in 0..6 {
-                // Interiour lines
-                // draw line from center
-                let n = h.neighbor(kite);
-                let midpoint = avg(center, n.screen());
-                let nn = h.neighbor((kite + 1) % 6);
-                let corner = avg3(center, n.screen(), nn.screen());
+    fn hat_is_empty(&self, h: HexPos, kite: usize) -> bool {
+        let n1 = h.neighbor((kite + 1) % 6);
+        let n2 = h.neighbor((kite + 2) % 6);
 
-                // XXX Imagine a brilliant kite painting code
-                // drawing the center-corner-midpoint triangle
-                // instead of this
-                //pict.draw_triangle(center,corner,midpoint, GREY);
-                if hex[h.0 as usize][h.1 as usize][kite] != EMPTY {
-                    let kite_center = avg3(center, corner, midpoint);
-                    {
+        self.get(h, kite) == EMPTY
+            && self.get(h, (kite + 1) % 6) == EMPTY
+            && self.get(n1, (kite + 2) % 6) == EMPTY
+            && self.get(n1, (kite + 3) % 6) == EMPTY
+            && self.get(n2, kite) == EMPTY
+            && self.get(n2, (kite + 3) % 6) == EMPTY
+            && self.get(n2, (kite + 4) % 6) == EMPTY
+            && self.get(n2, (kite + 5) % 6) == EMPTY
+    }
+
+    fn hat_set(&mut self, h: HexPos, kite: usize, c: KiteColor) {
+        let n1 = h.neighbor((kite + 1) % 6);
+        let n2 = h.neighbor((kite + 2) % 6);
+
+        self.set(h, kite, c);
+        self.set(h, (kite + 1) % 6, c);
+        self.set(n1, (kite + 2) % 6, c);
+        self.set(n1, (kite + 3) % 6, c);
+        self.set(n2, kite, c);
+        self.set(n2, (kite + 3) % 6, c);
+        self.set(n2, (kite + 4) % 6, c);
+        self.set(n2, (kite + 5) % 6, c);
+    }
+
+    fn try_position(&mut self, h: HexPos, kite: usize, budget: u8) {
+        if self.hat_is_empty(h, kite) {
+            self.hat_set(h, kite, budget);
+            self.search(budget - 1);
+            self.hat_set(h, kite, EMPTY);
+        }
+    }
+
+    fn search(&mut self, budget: u8) {
+        if budget == 0 {
+            /*
+            for y in 2..N as isize - 2 {
+                for x in 2..N as isize - 2 {
+                    let h = HexPos(x, y);
+
+                    for k in 0..6 {
+                        if self.get(h, k) == EMPTY {
+                            return;
+                        }
+                    }
+                }
+            }
+            */
+
+            //print!("{}[H", 27 as char);
+            self.display_tiling();
+            return;
+        }
+
+        for y in 2..N as isize - 2 {
+            for x in 2..N as isize - 2 {
+                for d in 0..6 {
+                    self.try_position(HexPos(x, y), d, budget);
+                }
+            }
+        }
+    }
+
+    fn new() -> Self {
+        Self {
+            hex: [[[EMPTY; 8]; N]; N],
+        }
+    }
+
+    fn display_tiling(&self) {
+        use iterm2canvas::Pict;
+        const WHITE: u32 = 0xFFFFFF;
+        const GREY: u32 = 0x7F7F7F;
+        const YELLOW: u32 = 0xFFFF00;
+
+        let mut pict = Pict::new(
+            N as isize * 2 * RADIUS * 866 / 1000,
+            N as isize * 2 * RADIUS,
+        );
+
+        // I just need the center point of hexagon h
+        // The midpoint between the neighbor h' and h AVG(h',h) is the middle of the line between them.
+        // Call the left neighbor of h and h' L(h,h') and R(h,h') then the avg of h, h', and L(
+
+        for y in 0..N as isize {
+            for x in 0..N as isize {
+                let h = HexPos(x, y);
+
+                pict.plot(h.display_center(), WHITE);
+                let center = h.display_center();
+
+                for kite in 0..6 {
+                    // Interiour lines
+                    // draw line from center
+                    let n = h.neighbor(kite);
+                    let nn = h.neighbor((kite + 1) % 6);
+                    let corner = avg3(center, n.display_center(), nn.display_center());
+                    let midpoint = avg(center, n.display_center());
+
+                    // XXX Imagine a brilliant kite painting code
+                    // drawing the center-corner-midpoint triangle
+                    // instead of this
+                    //pict.draw_triangle(center,corner,midpoint, GREY);
+                    if self.get(h, kite) != EMPTY {
+                        let kite_center = avg3(center, corner, midpoint);
                         for x in -3..=3 {
                             for y in -3..=3 {
                                 pict.plot((kite_center.0 + x, kite_center.1 + y), GREY);
                             }
                         }
                     }
-                }
 
-                if hex[h.0 as usize][h.1 as usize][kite]
-                    != hex[h.0 as usize][h.1 as usize][(kite + 5) % 6]
-                {
-                    // draw line from center
-                    let n = h.neighbor(kite);
-                    let midpoint = avg(center, n.screen());
-                    pict.draw_line(center, midpoint, YELLOW);
-                }
+                    if self.get(h, kite) != self.get(h, (kite + 5) % 6) {
+                        // draw line from center
+                        pict.draw_line(center, midpoint, YELLOW);
+                    }
 
-                // Exterior lines
-                let n = h.neighbor(kite);
-                if 0 <= n.0
-                    && n.0 < N
-                    && 0 <= n.1
-                    && n.1 < N
-                    && hex[h.0 as usize][h.1 as usize][kite]
-                        != hex[n.0 as usize][n.1 as usize][(kite + 2) % 6]
-                {
-                    pict.draw_line(midpoint, corner, YELLOW);
-                }
-            }
-        }
-    }
-
-    pict.dump_iterm2_image(Some(100));
-}
-
-fn get(hex: &mut [[[KiteColor; 6]; N as usize]; N as usize], h: Hex, kite: usize) -> KiteColor {
-    let Hex(x, y) = h;
-
-    if (0..N).contains(&x) && (0..N).contains(&y) {
-        hex[x as usize][y as usize][kite]
-    } else {
-        !0 // Some non-empty color
-    }
-}
-
-fn set(hex: &mut [[[KiteColor; 6]; N as usize]; N as usize], h: Hex, kite: usize, c: KiteColor) {
-    let Hex(x, y) = h;
-    if (0..N).contains(&x) && (0..N).contains(&y) {
-        hex[x as usize][y as usize][kite] = c;
-    } else {
-        panic!("set({h:?})");
-    }
-}
-
-fn hat_is_empty(hex: &mut [[[KiteColor; 6]; N as usize]; N as usize], h: Hex, kite: usize) -> bool {
-    let n1 = h.neighbor((kite + 1) % 6);
-    let n2 = h.neighbor((kite + 2) % 6);
-
-    get(hex, h, kite) == EMPTY
-        && get(hex, h, (kite + 1) % 6) == EMPTY
-        && get(hex, n1, (kite + 2) % 6) == EMPTY
-        && get(hex, n1, (kite + 3) % 6) == EMPTY
-        && get(hex, n2, kite) == EMPTY
-        && get(hex, n2, (kite + 3) % 6) == EMPTY
-        && get(hex, n2, (kite + 4) % 6) == EMPTY
-        && get(hex, n2, (kite + 5) % 6) == EMPTY
-}
-
-fn hat_set(
-    hex: &mut [[[KiteColor; 6]; N as usize]; N as usize],
-    h: Hex,
-    kite: usize,
-    c: KiteColor,
-) {
-    let n1 = h.neighbor((kite + 1) % 6);
-    let n2 = h.neighbor((kite + 2) % 6);
-
-    set(hex, h, kite, c);
-    set(hex, h, (kite + 1) % 6, c);
-    set(hex, n1, (kite + 2) % 6, c);
-    set(hex, n1, (kite + 3) % 6, c);
-    set(hex, n2, kite, c);
-    set(hex, n2, (kite + 3) % 6, c);
-    set(hex, n2, (kite + 4) % 6, c);
-    set(hex, n2, (kite + 5) % 6, c);
-}
-
-fn try_position(
-    hex: &mut [[[KiteColor; 6]; N as usize]; N as usize],
-    h: Hex,
-    kite: usize,
-    budget: usize,
-) {
-    if hat_is_empty(hex, h, kite) {
-        hat_set(hex, h, kite, budget);
-
-        search(hex, budget - 1);
-
-        hat_set(hex, h, kite, EMPTY);
-    }
-}
-
-fn search(hex: &mut [[[KiteColor; 6]; N as usize]; N as usize], budget: usize) {
-    if budget == 0 {
-        for y in 2..N - 2 {
-            for x in 2..N - 2 {
-                let h = Hex(x, y);
-
-                for k in 0..6 {
-                    if get(hex, h, k) == EMPTY {
-                        return;
+                    // Exterior lines
+                    if self.valid(n) && self.get(h, kite) != self.get(n, (kite + 2) % 6) {
+                        pict.draw_line(midpoint, corner, YELLOW);
                     }
                 }
             }
         }
 
-        //print!("{}[H", 27 as char);
-        plot_hex(hex);
-        return;
-    }
-
-    for y in 2..N - 2 {
-        for x in 2..N - 2 {
-            for d in 0..6 {
-                try_position(hex, Hex(x, y), d, budget);
-            }
-        }
+        pict.dump_iterm2_image(None);
     }
 }
 
 fn main() {
-    let mut hex = [[[EMPTY; 6]; N as usize]; N as usize];
-
     /*
-        // Highlight a hat
-        hex[2][2][1] = '*';
-        hex[2][2][2] = '*';
-        hex[3][2][4] = '*';
-        hex[3][2][3] = '*';
-        hex[2][3][0] = '*';
-        hex[2][3][1] = '*';
-        hex[2][3][4] = '*';
-        hex[2][3][5] = '*';
-    */
-
-    for budget in 1.. {
-        println!("Budget {budget}");
-        search(&mut hex, budget);
+    for o in 0..6 {
+        let mut kitegrid = KiteGrid::<6>::new();
+        kitegrid.hat_set(HexPos(2, 2), o, 1);
+        kitegrid.hat_set(HexPos(3, 3), 1, 2);
+        kitegrid.display_tiling();
     }
-
-    /*
-        for y in 1..N {
-            for yy in 0..6usize {
-                // 0.. -> 3,2,1,0,1,2,
-                let spaces = if yy <= 3 { 3 - yy } else { yy - 3 };
-                for _ in 0..spaces {
-                    print!(" ");
-                }
-
-                for x in 0..N {
-                    let e = if x % 2 == 0 { hex[y][x] } else { hex[y - 1][x] };
-
-                    match (yy + (x % 2) * 3) % 6 {
-                        0 => print!(">---"),
-                        1 => print!("/{} | {}", e[5], e[0]),
-                        2 => print!("/ \\ | / "),
-                        3 => print!("<{}   +   {}", e[4], e[1]),
-                        4 => print!("\\ / | \\ "),
-                        5 => print!("\\{} | {}", e[3], e[2]),
-                        x => panic!("{x} should have been impossible!"),
-                    }
-                }
-                println!();
-            }
-        }
     */
+
+    let mut kitegrid = KiteGrid::<8>::new();
+    for budget in 8..=255 {
+        println!("Budget {budget}");
+        kitegrid.search(budget);
+    }
 }
